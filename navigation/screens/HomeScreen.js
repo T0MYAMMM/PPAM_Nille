27d20917259ec5c535c66185cbcb424574f62106
Thomas Stefen Mardianto <18220106@std.stei.itb.ac.id>
2023-03-16 11:20:07 +0700

    Menambahkan container untuk background

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,12 +1,43 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function HomeScreen({ navigation }) {
+
+function HomeScreen(){
+    const navigation = useNavigation();
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
-        </View>
-    );
+        <LinearGradient colors={['#FFB6C1', '#87CEFA']} style={styles.container}>
+            <Text onPress={() => alert("Udah di Home Screen")}
+                style={{ fontSize: 26, fontWeight: 'bold' }}>Home screen</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+                <View style = {styles.button}>
+                <Text style={styles.buttonText}>Settings</Text>
+                </View>
+            </TouchableOpacity>
+        </LinearGradient>  
+  );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button: {
+      borderWidth: 2,
+      borderRadius: 10,
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      marginTop: 20,
+    },
+    buttonText: {
+      fontSize: 18,
+      color: '#29496F',
+      fontWeight: 'bold',
+    },
+  });
+
+export default HomeScreen;
