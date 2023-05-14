4546f9900e91f1711ca927225d78b07bfdd79fbb
Thomas Stefen Mardianto <18220106@std.stei.itb.ac.id>
2023-05-14 21:37:47 +0700

    fix navigaiton route, prepare add drawer

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,32 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { FontAwesome5 } from '@expo/vector-icons';
-import { get, ref } from 'firebase/database';
-import { db } from '../../firebaseConfig'; 
+
 
 // Screen
 import HomeScreen from '../screens/HomeScreen';
 import MyAquariumScreen from '../screens/MyAquariumScreen';
 import SearchScreen from '../screens/SearchScreen';
 import PremiumScreen from '../screens/PremiumScreen';
-import SignInScreen from '../screens/SignInScreen';
-import SignUpScreen from '../screens/SignUpScreen';
-import ConfirmEmailScreen from '../screens/ConfirmEmailScreen';
-import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
-import ResetPasswordScreen from '../screens/ResetPasswordScreen';
 import ChatBotScreen from '../screens/ChatBotScreen';
-import GetStartedScreen from '../screens/GetStartedScreen/GetStartedScreen';
-import UploadImageScreen from '../screens/UploadImageScreen/UploadImageScreen';
-import DetailFishScreen from '../screens/DetailFishScreen';
 //import DrawerScreen from '../screens/DrawerScreen/DrawerScreen';
 
-import ImageUploadContext from '../screens/ImageUploadContext';
 import ScreenHeader from '../components/ScreenHeader/ScreenHeader';
 import { useNavigation } from '@react-navigation/native';
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
@@ -125,3 +113,5 @@ const BottomTabNavigator = () => {
     </Tab.Navigator>
   );
 };
+
+export default BottomTabNavigator;
